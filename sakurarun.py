c70be749686961aae6064ca5ffea92a60ae07d73
LukaLambrecht <lukalambrecht@hotmail.com>
2024-02-23 21:08:25 +0100

    add csv reduction tools and tsp tools

--- sakurarun.py
+++ sakurarun.py
@@ -4,6 +4,7 @@
 
 
 # local imports
+import numpy as np
 import pandas as pd
 import requests
 # import GraphHopper API key
@@ -15,6 +16,7 @@ from route import get_route_coords
 from route import plot_route_coords
 from tools.coordtools import df_to_coords
 from tools.kmltools import coords_to_kml
+from tools.tsptools import solve_tsp
 
 
 if __name__=='__main__':
@@ -22,6 +24,7 @@ if __name__=='__main__':
     # load input file (test file for now)
     inputfile = 'data/test.csv'
     df = pd.read_csv(inputfile, delimiter=';')
+    print('Read input file {} with {} entries.'.format(inputfile, len(df)))
 
     # get coordinates in suitable format
     coords = df_to_coords(df)
@@ -30,18 +33,31 @@ if __name__=='__main__':
     session = requests.Session()
 
     # calculate distance matrix
+    print('Calculating distance matrix...')
     distances = get_distance_matrix(coords, session=session, profile='foot')
     plot_distance_matrix(coords, distances=distances)
 
     # optimization of route
-    # to be done, dummy for now
-    ids = list(range(len(coords)))
+    print('Finding shortest path...')
+    (ids, dist) = solve_tsp(distances, method='local')
+    coords = [coords[idx] for idx in ids]
+    print('Shortest path: {:.3f} km'.format(dist/1000))
+	
+	# cross-check with other heuristic methods
+    check_methods = ['annealing']
+    threshold = 0.05
+    print('Cross-checking result...')
+    for method in check_methods:
+	    (_, check_dist) = solve_tsp(distances, method=method)
+	    if np.abs(dist-check_dist)/dist > threshold:
+		    msg = 'WARNING: found more than {}% deviation in cross-check.'.format(threshold*100)
+		    print(msg)
 
     # calculate route
     (route_coords, route_info) = get_route_coords(coords, session=session, profile='foot')
     
     # print some info and make plot
-    print('Total distance: {} km'.format(route_info['distance']/1000))
+    print('Total distance: {:.3f} km'.format(route_info['distance']/1000))
     plot_route_coords(coords, route_coords=route_coords)
 
     # write output KML file (e.g. for use in google maps)
---
+++ tools/tsptools.py
@@ -0,0 +1,26 @@
+import numpy as np
+import python_tsp
+from python_tsp.exact import solve_tsp_dynamic_programming
+from python_tsp.heuristics import solve_tsp_local_search
+from python_tsp.heuristics import solve_tsp_simulated_annealing
+
+
+def solve_tsp(distances, method='exact'):
+	# solve the traveling salesperson problem for a given distance matrix
+	# input arguments:
+	# - distances: square np array with distances
+	# - method: choose from 'exact', 'local' or 'annealing'
+	# returns:
+	#   a tuple with the shortes path indices and distance
+	if method=='exact':
+	    shortest_path_inds, shortest_path_dist = solve_tsp_dynamic_programming(distances)
+	elif method=='local':
+	    shortest_path_inds, shortest_path_dist = solve_tsp_local_search(distances)
+	elif method=='annealing':
+		shortest_path_inds, shortest_path_dist = solve_tsp_simulated_annealing(distances)
+	else:
+		msg = 'Method "{}" not recognized.'.format(method)
+		raise Exception(msg)
+	# add the first index to the end to make the closed loop explicit
+	shortest_path_inds = shortest_path_inds + [shortest_path_inds[0]]
+	return (shortest_path_inds, shortest_path_dist)
